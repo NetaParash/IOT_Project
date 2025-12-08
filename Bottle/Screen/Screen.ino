e5dee164d6e97afd87a54a57a19f0aed8f1ba3d5
orzohar <or.zohar@campus.technion.ac.il>
2025-12-08 17:34:07 +0200

    fix screen class

--- Bottle/Screen/Screen.ino
+++ Bottle/Screen/Screen.ino
@@ -1,21 +1,37 @@
-class Screen{
-  HardwareSerial& serial;
-  public:
-    Screen(HardwareSerial& s) : serial(s){};
-  void setup() {
-          // Start OLED
-          display.begin(OLED_ADDR, true);
-          display.clearDisplay();
-          display.setTextSize(2);
-          display.setTextColor(SH110X_WHITE);
-          display.setCursor(0, 0);
-  }    
-  void print(string text){
-    display.clearDisplay();
-    display.setCursor(0, 0);
-    display.setTextSize(2);
-    display.println(text);
-    display.display();
-  }
-  
-}
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SH110X.h>
+
+class Screen {
+private:
+    HardwareSerial& serial;
+    int sdaPin, sclPin;
+    Adafruit_SH110X display = Adafruit_SH110X(128, 64, &Wire);
+    const uint8_t OLED_ADDR = 0x3C;
+
+public:
+    Screen(HardwareSerial& s, int sda, int scl)
+        : serial(s), sdaPin(sda), sclPin(scl) {}
+
+    void setup() {
+        // Start I2C on given pins
+        Wire.begin(sdaPin, sclPin);
+
+        // Start OLED
+        display.begin(OLED_ADDR, true);
+        display.clearDisplay();
+        display.setTextSize(2);
+        display.setTextColor(SH110X_WHITE);
+        display.setCursor(0, 0);
+
+        serial.println("Screen initialized.");
+    }
+
+    void print(String text) {
+        display.clearDisplay();
+        display.setCursor(0, 0);
+        display.setTextSize(2);
+        display.println(text);
+        display.display();
+    }
+};
