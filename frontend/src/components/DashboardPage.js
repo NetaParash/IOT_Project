db1be907a41dd815065f6010c5281f01c9819b9a
orzohar <or.zohar@campus.technion.ac.il>
2026-01-05 11:52:58 +0200

    fix resoloution on the dashboard screen

--- frontend/src/components/DashboardPage.js
+++ frontend/src/components/DashboardPage.js
@@ -9,29 +9,35 @@ export default function DashboardPage() {
 
     return (
         <Grid container spacing={3}>
-            {/* ===== Row 1: 3 cards ===== */}
-                <Grid item size={3}>
-                    <DashboardCard title="Water Level" subtitle="Current">
-                        <WaterLevel />
-                    </DashboardCard>
-                </Grid>
+            {/* ===== Row 1 ===== */}
+            <Grid item xs={12} md={4}>
+                <DashboardCard title="Water Level" subtitle="Current">
+                    <WaterLevel />
+                </DashboardCard>
+            </Grid>
 
-                <Grid item size={3}>
-                    <DashboardCard title="Daily Goal" subtitle="It's important to stay hydrated">
-                        <Typography variant="h1">{goal} ml</Typography>
-                    </DashboardCard>
-                </Grid>
+            <Grid item xs={12} md={4}>
+                <DashboardCard
+                    title="Daily Goal"
+                    subtitle="It's important to stay hydrated"
+                >
+                    <Typography variant="h1">{goal} ml</Typography>
+                </DashboardCard>
+            </Grid>
 
-                <Grid item size={3}>
-                    <DashboardCard title="My Progress" subtitle={`Today drank - ${drankToday} ml`}>
-                        <Typography variant="h1">
-                            {Math.round((drankToday * 100) / goal)} %
-                        </Typography>
-                    </DashboardCard>
-                </Grid>
+            <Grid item xs={12} md={4}>
+                <DashboardCard
+                    title="My Progress"
+                    subtitle={`Today drank - ${drankToday} ml`}
+                >
+                    <Typography variant="h1">
+                        {Math.round((drankToday * 100) / goal)} %
+                    </Typography>
+                </DashboardCard>
+            </Grid>
 
-            {/* ===== Row 2: Graph ===== */}
-            <Grid item size={9}>
+            {/* ===== Row 2 ===== */}
+            <Grid item xs={12}>
                 <DashboardCard title="Drinking Amount" subtitle="Today">
                     <DrinkingAmountGraph />
                 </DashboardCard>
